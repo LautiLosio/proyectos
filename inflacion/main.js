a94f66b631fa6d2402ad27e30b0bc02947645616
Lauti Losio <lautarolosio@gmail.com>
2023-06-10 02:18:13 -0300

    colores

--- inflacion/main.js
+++ inflacion/main.js
@@ -1,25 +1,23 @@
 MicroModal.init();
 
 hasInteres = document.querySelectorAll('input[name="has-interes"]');
-interesInput = document.querySelector('#interes');
+interesInput = document.querySelector("#interes");
 
 hasInteres.forEach(function (input) {
-  input.addEventListener('change', function () {
+  input.addEventListener("change", function () {
     console.log(input.value);
-    if (input.value === 'true') {
-      interesInput.removeAttribute('disabled');
-      interesInput.classList.remove('disabled');
-    }
-    else {
-      interesInput.setAttribute('disabled', 'disabled');
-      interesInput.classList.add('disabled');
+    if (input.value === "true") {
+      interesInput.removeAttribute("disabled");
+      interesInput.classList.remove("disabled");
+    } else {
+      interesInput.setAttribute("disabled", "disabled");
+      interesInput.classList.add("disabled");
     }
   });
 });
 
-calcuarBtn = document.querySelector('#calcular-btn');
-calcularBtn.addEventListener('click', function (e) {
+calcuarBtn = document.querySelector("#calcular-btn");
+calcularBtn.addEventListener("click", function (e) {
   e.preventDefault();
-  MicroModal.show('modal-1');
+  MicroModal.show("modal-1");
 });
-
